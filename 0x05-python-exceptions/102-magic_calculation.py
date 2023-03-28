b573204c21416ba29687d782184dbb56a523075c
Dayortobby <dayortobby@gmail.com>
2023-03-28 00:44:00 +0000

    update

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,15 +1,14 @@
+#!/usr/bin/python3
+
 def magic_calculation(a, b):
     result = 0
-    for i in range(1, 4):
+    for i in range(1, 3):
         try:
             if i > a:
                 raise Exception('Too far')
-            result += (a ** b) / i
+            else:
+                result += a ** b / i
         except:
-            pass
-        else:
-            continue
-        finally:
+            result = b + a
             break
-    result += a + b
-    return result
+    return (result)
