1205d248ef48065cd73bc57db86f2eb13c1a27ae
Dayortobby <dayortobby@gmail.com>
2023-03-30 04:14:44 +0000

    update

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -7,8 +7,9 @@ class Square:
 
     def __init__(self, size=0):
         """
+        initializes square
         Args:
-            size: size of square
+            size: size of side of square
         """
 
         if type(size) is int:
@@ -18,3 +19,12 @@ class Square:
                 self.__size = size
         else:
             raise TypeError('size must be an integer')
+
+    def area(self):
+        """
+        finds area of square
+        Returns:
+            the area of the square
+        """
+
+        return self.__size ** 2
