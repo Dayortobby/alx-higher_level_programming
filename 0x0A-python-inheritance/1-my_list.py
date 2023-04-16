b0def0a23243587e77d84122b9deb2cd0e15c19b
Dayortobby <dayortobby@gmail.com>
2023-04-16 23:11:49 +0000

    1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -9,8 +9,7 @@ class MyList(list):
     def __init__(self):
         """initiates the object"""
         super().__init__()
-        """ invokes the __init__ method of list"""
-    
+        
     def print_sorted(self):
         """prints out the sorted list"""
         print(sorted(self))
