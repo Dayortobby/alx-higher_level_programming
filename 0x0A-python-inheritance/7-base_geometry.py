89b215374bafb59c994157d6bd78a8100aae1163
Dayortobby <dayortobby@gmail.com>
2023-04-17 17:03:22 +0000

    7-base_geometry

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+contains a class BaseGeometry
+"""
+class BaseGeometry(object):
+    """intiating the class methods"""
+    def __init__(self):
+        pass
+    """raises an Exception"""
+    def area(self):
+        raise Exception("area() is not implemented")
+    """validates value"""
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
