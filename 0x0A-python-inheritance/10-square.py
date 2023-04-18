1f28084a6395d1343083226d32a838ec47bbd014
Dayortobby <dayortobby@gmail.com>
2023-04-18 18:48:40 +0000

    10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+contains a subclass Square that inherits from Rectangle
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """Representing a Square"""
+    def __init__(self, size):
+    """instantiation  with size"""
+        self.
+self.__size = size
+        
