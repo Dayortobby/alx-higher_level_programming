abb45a5e2d4ccbbf084dbd4cedec64ca80fe657c
Dayortobby <dayortobby@gmail.com>
2023-04-20 12:03:30 +0000

    100-MyInt

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,9 +5,7 @@ Contains the class MyInt
 
 
 class MyInt(int):
-    """
-    A rebel int that inverts the == and != operators.
-    """
+    """A rebel int that inverts the == and != operators."""
     def __eq__(self, other):
         """Return the opposite of == operator."""
         return super().__ne__(other)
