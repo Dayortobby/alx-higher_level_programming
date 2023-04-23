b82eabb2c9755925a2c789a58dbda44c29411d21
Dayortobby <dayortobby@gmail.com>
2023-04-23 09:06:36 +0000

    3. To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+contains function "to_json_string"
+"""
+
+
+def to_json_string(my_obj):
+    """""returns the JSON representation of an object (string)"""
+    
---
+++ 3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+contains to_json_string function
+"""
+
+
+def to_json_string(my_obj):
+    """""returns the JSON representation of an object (string)"""
+    return json.dumps(obj)
