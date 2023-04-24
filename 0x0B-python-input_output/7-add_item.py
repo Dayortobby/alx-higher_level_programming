dedcb58713c0d994bd09b35f010bc9b383fe61dc
Dayortobby <dayortobby@gmail.com>
2023-04-24 10:42:36 +0000

    7. Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,21 +3,18 @@
 Script that adds all arguments to a Python list, and then saves them to a file
 """
 
-import sys
-save_to_json_file = __import__("5-save_to_json_file.py").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file.py").load_from_json_file
+from sys import argv
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 filename = "add_item.json"
 
-"""load existing list from the file, or create an empty list"""
 try:
     json_list = load_from_json_file(filename)
 except FileNotFoundError:
     json_list = []
 
-"""Add the command line arguments to the list"""
-for arg in sys.argv[1:]:
+for arg in argv[1:]:
     json_list.append(arg)
 
-"""Save the updated list to the file"""
 save_to_json_file(json_list, filename)
