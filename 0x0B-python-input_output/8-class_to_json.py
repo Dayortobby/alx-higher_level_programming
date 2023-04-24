b4d91dec05a51f17c093cdea47115fe127e22806
Dayortobby <Dayortobby@gmail.com>
2023-04-24 21:13:40 +0000

    8. Class to JSON

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-contains class_to_json function
+Contains class_to_json function
 """
 
 
@@ -8,4 +8,4 @@ def class_to_json(obj):
     """returns dictionary description with simple data structure 
     (list, dictionary, string, integer and boolean)
     for json serialisation of an object"""
-    returns obj.__dict__
+    return obj.__dict__
