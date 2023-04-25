5bc9d19a6ca4f86a8c15b62a68c565b4dbe74c90
Dayortobby <Dayortobby@gmail.com>
2023-04-25 06:59:19 +0000

    10. Student to JSON with filter

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -7,12 +7,7 @@ Contains class Student
 class Student(object):
     """Rep a student"""
     def __init__(self, first_name, last_name, age):
-        """Initialises a new student
-        Args:
-            first_name (str): The first name of the student
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
+        """Initialises a new student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
@@ -21,13 +16,8 @@ class Student(object):
         return obj.__dict__
 
     def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student.
-        If attrs is a list of strings, represents only those attributes
-        included in the list.
-        Args:
-            attrs (list): (Optional) The attributes to represent.
-        """
+        """Get a dictionary representation of the Student."""
         if (type(attrs) == list and
                  all(type(ele) == str for ele in attrs)):
-            return{k: getattr(self, k) for k in attrs if hasattr(sef, k)}
-        retutn self.__dict__
+            return{k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
