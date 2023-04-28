dc3f83c2273f74975044e8d0c086141553fbc6f1
Dayortobby <Dayortobby@gmail.com>
2023-04-28 08:04:23 +0000

    11. Student to disk and reload

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -13,16 +13,16 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-    """Get a dictionary representation of the student"""
-    if (type(attrs)) == list and
-            all(type(ele)) == str for ele in attrs)):
-        return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-    return self.__dict__
+        """Get a dictionary representation of the student"""
+        if (type(attrs)) == list and
+                all(type(ele)) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
 
     def reload_from_json(self, json):
-    """replaces all attributes of the Student"""
-    for key in json:
-        try:
-            setattr(selt, key, json[key])
-        except fileNotFounderror:
-            pass
+        """replaces all attributes of the Student"""
+        for key in json:
+            try:
+                setattr(selt, key, json[key])
+            except fileNotFounderror:
+                pass
