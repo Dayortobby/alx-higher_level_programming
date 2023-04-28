d07c11c7d2a0da98dc194baa716c922211f4e956
Dayortobby <Dayortobby@gmail.com>
2023-04-28 08:03:07 +0000

    11. Student to disk and reload

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -7,10 +7,10 @@ contains Student Class
 class Student:
     """Represent a Student"""
     def __init__(self, first_name, last_name, age):
-    """Initialise a new student"""
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
+        """Initialise a new student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self, attrs=None):
     """Get a dictionary representation of the student"""
