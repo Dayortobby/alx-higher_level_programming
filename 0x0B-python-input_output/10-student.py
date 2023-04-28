a234b43f9998fdc0f67e5b7263bff027e1c1273f
Dayortobby <Dayortobby@gmail.com>
2023-04-28 09:23:56 +0000

    10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -15,6 +15,6 @@ class Student(object):
 
     def to_json(self, attrs=None):
         if (type(attrs) == list and
-                 all(type(ele) == str for ele in attrs)):
+                all(type(ele) == str for ele in attrs)):
             return{k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
