0cda4ba90cc3e4db598df2f59b19e35af2175569
Dayortobby <Dayortobby@gmail.com>
2023-04-28 09:08:12 +0000

    12. Pascal's Triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -6,16 +6,16 @@ Contains function "pascal_triangle"
 
 def pascal_triangle(n):
     """returns [] of []s of ints rep the Pascal’s tri of n"""
-    if n <= 0
+    if n <= 0:
         return []
 
     triangle = [[1]]
     for i in range(1, n):
-        prev_row = [-1]
+        prev_row = triangle[-1]
         curr_row = [1]
-        for j in range(1, i)
+        for j in range(1, i):
             curr_row.append(prev_row[j-1] + prev_row[j])
         curr_row.append(1)
         triangle.append(curr_row)
 
-     return triange
+    return triangle
