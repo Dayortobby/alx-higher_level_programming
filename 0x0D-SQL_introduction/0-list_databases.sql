5d4f5ca652fd57764cbd4969090411d969d06b08
Dayortobby <Dayortobby@gmail.com>
2023-05-18 23:57:13 +0000

    0. List databases

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,9 +1,4 @@
 #!/bin/bash
--- lists all databases of your MySQL server.
-# Prompt for MySQL root password
-read -s -p "Enter MySQL root password: " mysql_password
-echo
-
-# Execute the SQL query to list databases
+-- Execute the SQL query to list databases
 query="SHOW DATABASES;"
 mysql -h localhost -u root -p"${mysql_password}" -e "${query}"
