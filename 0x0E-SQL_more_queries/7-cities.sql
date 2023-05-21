f428f1ebcd8f8dbbe869845338d1cd874cace621
Dayortobby <Dayortobby@gmail.com>
2023-05-21 12:12:32 +0000

    7. Cities table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,4 +5,6 @@ CREATE TABLE IF NOT EXISTS `cities` (
     `id`       INT          NOT NULL AUTO_INCREMENT,
     `state_id` INT          NOT NULL FOREIGN KEY(`id`),
     `name`     VARCHAR(256) NOT NULL,
+    FOREIGN KEY(`state_id`)
+        REFERENCES `hbtn_0d_usa`.`state_id`
 );
