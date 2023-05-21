de14b674e9e67424ccc1bf0c4412743ed9530d00
Dayortobby <Dayortobby@gmail.com>
2023-05-21 08:54:30 +0000

    2. Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- creates the database hbtn_0d_2 and the user user_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_2 IDENTIFIED BY 'user_0d_2_pwd';
-GRANT user_0d_2 SELECT ON * TO user_0d_2;
+GRANT user_0d_2 SELECT ON `hbtn_0d_2`* TO `user_0d_2`;
