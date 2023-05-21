fe8f3ed2f42ea4722655c9bdee641d41f3086239
Dayortobby <Dayortobby@gmail.com>
2023-05-21 11:32:31 +0000

    6. States table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -3,5 +3,5 @@ CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
     PRIMARY KEY(`id`),
     `id`   INT          NOT NULL AUTO_INCREMENT,
-    `nam VARCHAR(256) NOT NULL
+    `name` VARCHAR(256) NOT NULL
 );
