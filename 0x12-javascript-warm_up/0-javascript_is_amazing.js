4c8ff25f65b843e4da212c04562d1133accf3a51
Dayortobby <Dayortobby@gmail.com>
2023-06-19 19:56:38 +0000

    update

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 // Create a constant variable called myVar
-const myVar = "JavaScript is amazing";
+const myVar = 'JavaScript is amazing';
 
 // Print the output using console.log(...)
 console.log(myVar);
