7b71e92fd5552363de678dbc8419e0e0d441c06b
Dayortobby <Dayortobby@gmail.com>
2023-06-21 11:55:03 +0000

    update

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,6 +3,8 @@
 // Check if two arguments were passed
 if (process.argv[2] !== undefined && process.argv[3] !== undefined) {
   console.log(`${process.argv[2]} is ${process.argv[3]}`);
+} else if (process.argv[2] !== undefined) {
+  console.log(`${process.argv[2]} is undefined`);
 } else {
   console.log("undefined is undefined");
 }
