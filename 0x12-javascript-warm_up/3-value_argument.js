eb163e783082d361e5cbe77f20ed77494cb590a5
Dayortobby <Dayortobby@gmail.com>
2023-06-21 10:44:36 +0000

    update

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,7 +2,7 @@
 
 // a script checking if argument was passed to it
 if (process.argv[2] === undefined ) {
-  console.log('No argument')
+  console.log('No argument');
 } else {
-  console.log(process.argv[2])
+  console.log(process.argv[2];)
 }
