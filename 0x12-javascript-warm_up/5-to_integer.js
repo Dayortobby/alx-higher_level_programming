0b547aa3478dc8cd4e81458fcd5566f3a058efb7
Dayortobby <Dayortobby@gmail.com>
2023-06-21 12:54:13 +0000

    update

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -10,7 +10,6 @@ if (isNaN(number)) {
   console.log(`My number: ${number}`);
 }
 
-
 /* #!/usr/bin/node
 const num = Math.floor(Number(process.argv[2]));
 console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`); */
