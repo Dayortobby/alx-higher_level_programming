06a14bc57278f552cd16703674dfd2c3c0473185
Dayortobby <Dayortobby@gmail.com>
2023-06-21 12:46:36 +0000

    update

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/*prints My number: <first argument converted in integer> if the first argument can be converted to an integer*/
+
+const number = parseInt(process.argv[2]);
+
+if (isNaN(number)) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${number}`);
+}
